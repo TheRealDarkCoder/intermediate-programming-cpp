cff7606be15343fa37c67ae24b3d34323f339922
TheRealDarkCoder <zuhayr2003@gmail.com>
2025-04-07 13:59:01 +0300

    Exercise 5 done

--- Lab4_Inheritence/lab4_inheritance/flower.h
+++ Lab4_Inheritence/lab4_inheritance/flower.h
@@ -9,13 +9,16 @@
 #define FLOWER_H_
 
 #include "shape.h"
+#include "rectangle.h"
+#include "circle.h"
+#include "triangle.h"
 
 class Flower : public Drawable
 {
 	private:
-		Shape stem;
-		Shape pistil;  // center piece of flower
-		Shape leaf;
+		Rectangle stem;
+		Circle pistil;  // center piece of flower
+		Triangle leaf;
 
 		void drawPedals(PNG& canvas, const Vector2& center, int x, int y) const;
 	public:
