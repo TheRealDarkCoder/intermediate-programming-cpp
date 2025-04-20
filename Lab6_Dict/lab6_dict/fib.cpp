567e5ced9b71c5379a8cf80c65d59799a4d10773
TheRealDarkCoder <zuhayr2003@gmail.com>
2025-04-20 14:50:50 +0300

    Fib

--- Lab6_Dict/lab6_dict/fib.cpp
+++ Lab6_Dict/lab6_dict/fib.cpp
@@ -22,7 +22,15 @@ using std::map;
 unsigned long fib(unsigned long n)
 {
     /* Your code goes here! */
-    return 0;
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    
+    // Recursive case: F(n) = F(n-1) + F(n-2)
+    return fib(n - 1) + fib(n - 2);
 }
 
 /** 
@@ -34,5 +42,20 @@ unsigned long fib(unsigned long n)
 unsigned long memoized_fib(unsigned long n)
 {
     /* Your code goes here! */
-    return 0;
+    static map<unsigned long, unsigned long> memo = {
+        {0, 0},
+        {1, 1}
+    };
+    
+    // Check if we've already calculated this Fibonacci number
+    auto lookup = memo.find(n);
+    if (lookup != memo.end()) {
+        // Return the stored value if found
+        return lookup->second;
+    } else {
+        // Calculate the value using recursion, store it, and return it
+        unsigned long result = memoized_fib(n - 1) + memoized_fib(n - 2);
+        memo[n] = result;
+        return result;
+    }
 }
